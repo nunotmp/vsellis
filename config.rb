8a9393f8759d038f6dfea093502fb32b2c70815a
Juan Rangel <juan@vsellis.com>
2015-04-21 18:57:39 -0500

    Inital commit

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "."
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+relative_assets = true
+output_style = :nested
+line_comments = false
+preferred_syntax = :sass
